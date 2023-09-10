b268d602f03da6977cac375ab399727b2c36c3f3
Aden McCusker <aden.m@null.net>
2023-09-10 15:32:43 +1000

    Completed manual account linking flow

--- lib/page/landing_page.dart
+++ lib/page/landing_page.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:specon/page/verify_email_page.dart';
 
 import 'dashboard_page.dart';
 import 'loading_page.dart';
@@ -17,13 +18,19 @@ class Landing extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<User?>(
-      stream: _auth.authStateChanges(),
+      stream: _auth.userChanges(),
       builder: (context, snapshot) {
         if(snapshot.connectionState == ConnectionState.active) {
           final User? user = snapshot.data;
           if(user == null) {
             return const Login();
           }
+
+          if(!user.emailVerified) {
+            user.sendEmailVerification();
+            return const VerifyEmail();
+          }
+
           return const Dashboard(
             userType: UserType.student, // TODO: Get actual type from Canvas.
           );
---
+++ lib/page/verify_email_page.dart
@@ -0,0 +1,48 @@
+/// A page that waits for email verification to complete.
+
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class VerifyEmail extends StatefulWidget {
+  const VerifyEmail({Key? key}) : super(key: key);
+
+  @override
+  State<VerifyEmail> createState() => _VerifyEmailState();
+}
+
+class _VerifyEmailState extends State<VerifyEmail> {
+  static final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.doWhile(() async {
+      await Future.delayed(const Duration(seconds: 5));
+      await _auth.currentUser!.reload();
+      return !_auth.currentUser!.emailVerified;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(),
+              SizedBox(
+                width: 30,
+              ),
+              Text(
+                'A verification email has been sent to your email address.\n'
+                'Click the link in it to verify your account.',
+                textAlign: TextAlign.center,
+              )
+            ],
+          )
+      )
+    );
+  }
+}
