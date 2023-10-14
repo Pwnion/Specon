db0b9d6cb8512ad4a38b6a1f881ecdbd17394c3a
Jeremy <jeremya@outlook.com.au>
2023-10-14 13:19:16 +1100

    snakecase for db variables

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -3,24 +3,24 @@ import 'package:specon/user_type.dart';
 class UserModel {
   final String id;
   final String name;
-  final String emailAddress;
+  final String email;
   final List<dynamic> subjects;
-  final String? aap;
+  final String? aap_path;
 
   const UserModel({
     required this.id,
     required this.name,
-    required this.emailAddress,
+    required this.email,
     required this.subjects,
-    required this.aap,
+    required this.aap_path,
   });
 
   Map<String, dynamic> toJson() {
     return {
       'first_name': name,
-      'email': emailAddress,
+      'email': email,
       'subjects': subjects.toString(),
-      'aap': aap,
+      'aap': aap_path,
     };
   }
 }
