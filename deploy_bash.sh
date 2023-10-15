3022a668c518b415cce4540b04b5ad3f9f6da397
Aden McCusker <aden.m@null.net>
2023-10-15 17:25:44 +1100

    Fixed firebase hosting cache optimisation bug

--- deploy_bash.sh
+++ deploy_bash.sh
@@ -2,5 +2,5 @@
 flutter build web
 rm -rf functions/public
 mkdir functions/public
-mv -f build/web/{.[!.],}* functions/public/
-firebase deploy --only functions
+cp -r build/web/. functions/public
+firebase deploy --only hosting,functions
