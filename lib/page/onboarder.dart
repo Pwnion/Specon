a16bf659a716e0838e49cf5c121e61fb967a58fc
Jeremy <jeremya@outlook.com.au>
2023-10-25 15:41:39 +1100

    buttons in row

--- lib/page/onboarder.dart
+++ lib/page/onboarder.dart
@@ -11,13 +11,11 @@ class Onboarder extends StatefulWidget {
     required this.subject,
   }) : super(key: key);
 
-
   @override
   State<Onboarder> createState() => _OnboarderState();
 }
 
 class _OnboarderState extends State<Onboarder> {
-
   var permanButtonEnabled = false;
   var finishButtonEnabled = false;
 
@@ -32,81 +30,91 @@ class _OnboarderState extends State<Onboarder> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(widget.subject.name,
-                style: const TextStyle(color: Colors.green)),
-            Text(widget.subject.code,
-                style: const TextStyle(color: Colors.green)),
-            Text(widget.subject.semester,
-                style: const TextStyle(color: Colors.green)),
-            Text(widget.subject.year,
-                style: const TextStyle(color: Colors.green)),
-            TextButton(
-              onPressed: () {
-                setState(() {
-                  permanButtonEnabled = true;
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => AssessmentManager(
-                        subject: widget.subject,
-                        refreshFn: setState,
-                      ),
+        backgroundColor: Theme.of(context).colorScheme.background,
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(widget.subject.name,
+                  style: const TextStyle(color: Colors.green)),
+              Text(widget.subject.code,
+                  style: const TextStyle(color: Colors.green)),
+              Text(widget.subject.semester,
+                  style: const TextStyle(color: Colors.green)),
+              Text(widget.subject.year,
+                  style: const TextStyle(color: Colors.green)),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        permanButtonEnabled = true;
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (_) => AssessmentManager(
+                              subject: widget.subject,
+                              refreshFn: setState,
+                            ),
+                          ),
+                        );
+                      });
+                    },
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        side: const BorderSide(color: Colors.blue, width: 1),
+                      )),
                     ),
-                  );
-                });
-              },
-              style: ButtonStyle(
-                shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5),
-                  side: const BorderSide(color: Colors.blue, width: 1),
-                )),
-              ),
-              child: const Text('Assessment Manager'),
-            ),
-            TextButton(
-              onPressed: !isPermanButtonEnabled() ? null : () => {
-                setState(() {
-                  finishButtonEnabled = true;
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => PermissionManager(
-                        currentSubject: widget.subject,
-                      ),
+                    child: const Text('Assessment Manager'),
+                  ),
+                  TextButton(
+                    onPressed: !isPermanButtonEnabled()
+                        ? null
+                        : () => {
+                              setState(() {
+                                finishButtonEnabled = true;
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (_) => PermissionManager(
+                                      currentSubject: widget.subject,
+                                    ),
+                                  ),
+                                );
+                              })
+                            },
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        side: const BorderSide(color: Colors.blue, width: 1),
+                      )),
                     ),
-                  );
-                })},
-              style: ButtonStyle(
-                shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5),
-                  side: const BorderSide(color: Colors.blue, width: 1),
-                )),
+                    child: const Text('Permissions Manager'),
+                  ),
+                  TextButton(
+                    onPressed: !isFinishButtonEnabled()
+                        ? null
+                        : () => {
+                              setState(() {
+                                Navigator.pop(context);
+                              })
+                            },
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        side: const BorderSide(color: Colors.blue, width: 1),
+                      )),
+                    ),
+                    child: const Text('Finish'),
+                  ),
+                ],
               ),
-              child: const Text('Permissions Manager'),
-            ),
-
-        TextButton(
-          onPressed: !isFinishButtonEnabled() ? null : () => {
-            setState(() {
-              Navigator.pop(context);
-            })},
-
-          style: ButtonStyle(
-            shape: MaterialStateProperty.all(RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(5),
-              side: const BorderSide(color: Colors.blue, width: 1),
-            )),
+            ],
           ),
-          child: const Text('Finish'),)
-          ],
-        ),
-
-      ),
-    );
+        ));
   }
 }
