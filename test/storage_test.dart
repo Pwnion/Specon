9bd30c62b74ea4d53a7a8bf556d82e8b43210d4a
Lucas Chan <96561637+lucaschanzx@users.noreply.github.com>
2023-11-12 14:15:38 +1100

    Delete test/storage_test.dart

--- test/storage_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:specon/storage.dart';
-
-void storageUnitTest() {
-  test('storage test get aap name', () async {
-    final testResult = await getAapFileName("testGetAAP");
-    expect(testResult, "test.txt");
-  });
-}
